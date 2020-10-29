c51476f54618fd5423f08a50500cad78128f70e0
Rashed Khan Aupurbo <69986425+rkaupurbo@users.noreply.github.com>
2020-10-29 20:37:03 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+$('.counter').counterUp({
+    delay: 20,
+    time: 3000
+});
+
+
+// $(document).ready(function(){
+//     $(".sticker").sticky({topSpacing:20});
+//   });
+
+new WOW().init();
+
+
+//initiating jQuery
+              jQuery(function($) {
+                $(document).ready( function() {
+                  //enabling stickUp on the '.navbar-wrapper' class
+                  $('.navbar-wrapper').stickUp();
+                });
+              });
